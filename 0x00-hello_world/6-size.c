ed714a892c3329e0e9e9699605d2153a3df5550e
Abdo <nadia@nadia.com>
2023-03-18 17:19:38 -0700

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main fnc
+ *
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+
+printf("Size of char: %lu byte(s)\n", sizeof(a));
+printf("Size of int: %lu byte(s)\n", sizeof(b));
+printf("Size of long int: %lu byte(s)\n" sizeof(c));
+printf("Size of long long int: %lu byte(s)\n" sizeof(d));
+printf("Size of float: %lu byte(s)\n", sizeof(f));
+return (0);
+}
